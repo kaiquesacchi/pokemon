873e2e5370506931c9d206a6926d30fd48f55a1c
mac0321 26 <mac0321.26@metralha.cec.ime.usp.br>
2017-05-05 10:23:46 -0300

    Cria a primeira classe

---
+++ src/pokemon/Pokemon.java
@@ -0,0 +1,20 @@
+package pokemon;
+
+public class Pokemon {
+	//Atributos
+	private String nome;
+	private Status stats;
+	private AtkPool atkpool;
+	private String tipo;
+	private int id;
+	
+	//Construtor
+	Pokemon(String nome, int id, Status stats, AtkPool atkpool, String tipo){
+		this.nome = nome;
+		this.id = id;
+		this.stats = stats;
+		this.atkpool = atkpool;
+		this.tipo = tipo;
+	}
+	
+}
